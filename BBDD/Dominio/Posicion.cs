0b7995baec97d21c8a83c3cb3103ad7b7ffba70c
romero-ma17 <marcos.romero1@alu.uclm.es>
2026-03-09 16:10:00 +0100

    posicion y posicionDao

--- BBDD/Dominio/Posicion.cs
+++ BBDD/Dominio/Posicion.cs
@@ -6,5 +6,68 @@ namespace BBDD.Dominio
 {
     internal class Posicion
     {
+        public int Id { get; set; }
+        public Empleado empleado { get; set; }
+        public Oficina oficina { get; set; }
+        public String Name { get; set; }
+        public decimal YearSalary { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+        private readonly PosicionDao _dao;
+
+        public Posicion()
+        {
+            Id = 0;
+            empleado = new Empleado();
+            oficina = new Oficina();
+            Name = "";
+            YearSalary = 0;
+            StartDate = DateTime.Now;
+            EndDate = null;
+            _dao = new PosicionDao();
+        }
+
+        public Posicion(int idPosicion)
+        {
+            Id = idPosicion;
+            empleado = new Empleado();
+            oficina = new Oficina();
+            Name = "";
+            YearSalary = 0;
+            StartDate = DateTime.Now;
+            EndDate = null;
+            _dao = new PosicionDao();
+        }
+
+        public List<Posicion> ReadAllPosiciones()
+        {
+            return _dao.ReadAll();
+        }
+
+        public int InsertPosicion()
+        {
+            return _dao.Insert(this);
+        }
+
+        public int UpdatePosicion()
+        {
+            return _dao.Update(this);
+        }
+
+        public int DeletePosicion()
+        {
+            return _dao.Delete(this);
+        }
+
+        public bool CheckIfIdExists(String id)
+        {
+            return _dao.CheckIfIdExists(int.Parse(id));
+        }
+
+        public override string ToString()
+        {
+            return Id + " - " + Name;
+        }
     }
 }
--- BBDD/Dominio/PosicionDao.cs
+++ BBDD/Dominio/PosicionDao.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Company.persistence;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,81 @@ namespace BBDD.Dominio
 {
     internal class PosicionDao
     {
+        public PosicionDao() { }
+
+        public List<Posicion> ReadAll()
+        {
+            List<Posicion> resultado = new List<Posicion>();
+            string sql = "SELECT * FROM positions";
+            List<string[]> data = AgenteBD.GetInstance().Read(sql);
+
+            foreach (string[] row in data)
+            {
+                Posicion p = new Posicion(int.Parse(row[0]));
+                p.empleado = new Empleado(int.Parse(row[1]));
+                p.oficina = new Oficina(int.Parse(row[2]));
+                p.Name = row[3];
+                p.YearSalary = Convert.ToDecimal(row[4]);
+                p.StartDate = Convert.ToDateTime(row[5]);
+
+                if (string.IsNullOrEmpty(row[6]))
+                    p.EndDate = null;
+                else
+                    p.EndDate = Convert.ToDateTime(row[6]);
+
+                resultado.Add(p);
+            }
+
+            return resultado;
+        }
+
+        public int Insert(Posicion p)
+        {
+            string endDateSql = p.EndDate == null
+                ? "NULL"
+                : "'" + p.EndDate.Value.ToString("yyyy-MM-dd") + "'";
+
+            string sql = "INSERT INTO positions VALUES (" +
+                         p.Id + ", " +
+                         p.empleado.Id + ", " +
+                         p.oficina.Id + ", '" +
+                         p.Name + "', " +
+                         p.YearSalary + ", '" +
+                         p.StartDate.ToString("yyyy-MM-dd") + "', " +
+                         endDateSql + ")";
+
+            return AgenteBD.GetInstance().Change(sql);
+        }
+
+        public int Update(Posicion p)
+        {
+            string endDateSql = p.EndDate == null
+                ? "NULL"
+                : "'" + p.EndDate.Value.ToString("yyyy-MM-dd") + "'";
+
+            string sql = "UPDATE positions SET " +
+                         "posEmployee = " + p.empleado.Id + ", " +
+                         "posOffice = " + p.oficina.Id + ", " +
+                         "posName = '" + p.Name + "', " +
+                         "posYearSalary = " + p.YearSalary + ", " +
+                         "posStartDate = '" + p.StartDate.ToString("yyyy-MM-dd") + "', " +
+                         "posEndDate = " + endDateSql +
+                         " WHERE idPosition = " + p.Id;
+
+            return AgenteBD.GetInstance().Change(sql);
+        }
+
+        public int Delete(Posicion p)
+        {
+            string sql = "DELETE FROM positions WHERE idPosition = " + p.Id;
+            return AgenteBD.GetInstance().Change(sql);
+        }
+
+        internal bool CheckIfIdExists(int id)
+        {
+            string sql = "SELECT * FROM positions WHERE idPosition = " + id;
+            List<string[]> data = AgenteBD.GetInstance().Read(sql);
+            return data.Count > 0;
+        }
     }
 }
